890f8a622ed5402e06c9ca91110424883930e394
Aman Singh <144620401+Aman-pro-spec@users.noreply.github.com>
2025-11-23 13:45:52 +0530

    Update training.py

--- DiamondPricePrediction2/training.py
+++ DiamondPricePrediction2/training.py
@@ -4,15 +4,15 @@ from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
 
 if __name__ == "__main__":
-    # 1. Data Ingestion (Data read karega)
+    
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
-    # 2. Data Transformation (Data clean karke naya preprocessor.pkl banayega)
+    
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
-    # 3. Model Training (Naya model.pkl banayega jo naye version ke saath chalega)
+    
     model_trainer = ModelTrainer()
     model_trainer.initate_model_training(train_arr, test_arr)
     
